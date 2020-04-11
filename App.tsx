746401cf6ef06fbd8573e7ec59acba6dc19779f9
Luke Storry <luke@lukestorry.co.uk>
2020-04-11 14:51:51 +0100

    improved list styling with footer

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import {
   TextInput,
   TouchableHighlight,
   TouchableOpacity,
+  Dimensions,
 } from "react-native";
 
 import { config as fbConfig } from "./firebase";
@@ -40,25 +41,28 @@ export default function App() {
         <Image style={s.logo} source={require("./assets/icon.png")} />
         <Text style={s.headerText}> Eat Me </Text>
       </View>
+
       <FlatList
-        style={s.container}
-        data={Array.from({ length: 50 }).map((i) => ({ id: i }))}
+        style={s.list}
+        numColumns={Dimensions.get("window").width > 800 ? 2 : 1}
+        data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 121, 12, 13, 54, 60].map((i) => ({
+          key: i,
+          name: i,
+        }))}
         renderItem={({ item }) => (
-          <TouchableOpacity>
-            <View style={s.listItem}>
-              <Text style={{ fontWeight: "bold" }}>{`Item ${item.id} `}</Text>
+          <TouchableOpacity style={s.listItem}>
+            <View style={s.rowContainer}>
+              <Text style={{ fontWeight: "bold" }}>Item {item.name} </Text>
               <Text>{`Expires in ${Math.floor(Math.random() * 10)} days`}</Text>
             </View>
           </TouchableOpacity>
         )}
+        onRefresh={() => console.log("refreshing")}
+        refreshing={false}
       />
 
-      <View>
-        <TextInput
-          style={s.inputBox}
-          value={inputText}
-          onChangeText={setInputText}
-        />
+      <View style={s.inputBox}>
+        <TextInput value={inputText} onChangeText={setInputText} />
       </View>
     </>
   );
@@ -71,34 +75,38 @@ const s = StyleSheet.create({
     backgroundColor: "#42cdef",
     paddingBottom: 10,
     paddingLeft: 30,
-    paddingTop: Platform.OS == "web" ? 10 : 40,
+    paddingTop: Platform.OS === "web" ? 10 : 40,
   },
   headerText: {
     color: "#424242",
     fontSize: 40,
     fontWeight: "bold",
   },
-  container: {
-    padding: 30,
-    flex: 1,
-  },
   logo: {
     width: 50,
     height: 50,
   },
-  listItem: {
-    margin: 10,
-    height: 30,
-    backgroundColor: "#eaeaea",
-    padding: 20,
+  rowContainer: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-around",
+  },
+  list: {
+    height: 0,
+    marginHorizontal: "auto",
+    paddingHorizontal: 30,
+  },
+  listItem: {
+    padding: 10,
+    marginHorizontal: 40,
+    marginVertical: 10,
+    backgroundColor: "#eaeaea",
     borderRadius: 5,
     borderColor: "#424242",
     borderWidth: 1,
   },
   inputBox: {
+    alignItems: "center",
     backgroundColor: "#42cdef",
     padding: 20,
   },
